cb1d26c810c75e03f380d959074249ed396842c1
OlegPower <petkoo@ukr.net>
2020-09-22 20:36:54 +0300

    qwer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("pow3r");
+        System.out.println("pow3r");
+        System.out.println("pow3r");
+        System.out.println("pow3r");
     }
 }
